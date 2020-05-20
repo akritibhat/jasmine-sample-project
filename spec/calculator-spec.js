ccdec47f330893947fad9b053579aae6b12cdc12
Simha Reddy <rsimha@amazon.com>
2020-05-20 07:52:39 -0700

    final

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,4 +1,11 @@
 
+var reporters = require('jasmine-reporters');
+var junitReporter = new reporters.JUnitXmlReporter({
+    savePath: __dirname,
+    consolidateAll: false});
+
+ jasmine.getEnv().addReporter(junitReporter)
+
 
 describe('Calculator', function(){
     var Calculator = require('../Calculator');
